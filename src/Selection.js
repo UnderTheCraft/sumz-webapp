b7d1f599fae791cc38ff2b2c15c4ac41ecd71a90
Domenic Nauert <dome777@me.com>
2020-05-25 22:33:10 +0200

    Button disabled bei init

--- src/Selection.js
+++ src/Selection.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import { Layout } from './components/Layout';
 import Button from 'react-bootstrap/Button';
 import { Jumbotron } from './components/Jumbotron';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import DropdownItem from 'react-bootstrap/DropdownItem';
 import DropdownMenu from 'react-bootstrap/DropdownMenu';
 import DropdownToggle from 'react-bootstrap/DropdownToggle';
@@ -31,39 +31,41 @@ export class Selection extends React.Component {
     constructor(props) {
         super(props);
 
+        this.disableButton = true;
         this.currentId = 0;
         this.changeValue = this.changeValue.bind(this);
-        this.state = { actions:
-            [{ id: "1", key: "1", name: "adidas" },
-            { id: "2", key: "2", name: "Allianz" },
-            { id: "3", key: "3", name: "BASF" },
-            { id: "4", key: "4", name: "Bayer" },
-            { id: "5", key: "5", name: "Beiersdorf" },
-            { id: "6", key: "6", name: "BMW" },
-            { id: "7", key: "7", name: "Continental" },
-            { id: "8", key: "8", name: "Covestro" },
-            { id: "9", key: "9", name: "Daimler" },
-            { id: "10", key: "10", name: "Deutsche Bank" },
-            { id: "11", key: "11", name: "Deutsche Börse" },
-            { id: "12", key: "12", name: "Deutsche Post" },
-            { id: "13", key: "13", name: "Deutsche Telekom" },
-            { id: "14", key: "14", name: "E.ON" },
-            { id: "15", key: "15", name: "Fresenius" },
-            { id: "16", key: "16", name: "Fresenius Medical Care" },
-            { id: "17", key: "17", name: "HeidelbergCement" },
-            { id: "18", key: "18", name: "Henkel" },
-            { id: "19", key: "19", name: "Infineon" },
-            { id: "20", key: "20", name: "Linde" },
-            { id: "21", key: "21", name: "Lufthansa" },
-            { id: "22", key: "22", name: "Merck" },
-            { id: "23", key: "23", name: "MTU Aero Engines" },
-            { id: "24", key: "24", name: "Münchener Rück" },
-            { id: "25", key: "25", name: "RWE" },
-            { id: "26", key: "26", name: "SAP" },
-            { id: "27", key: "27", name: "Siemens" },
-            { id: "28", key: "28", name: "Volkswagen" },
-            { id: "29", key: "29", name: "Vonovia" },
-            { id: "30", key: "30", name: "Wirecard" }],
+        this.state = {
+            actions:
+                [{ id: "1", key: "1", name: "adidas" },
+                { id: "2", key: "2", name: "Allianz" },
+                { id: "3", key: "3", name: "BASF" },
+                { id: "4", key: "4", name: "Bayer" },
+                { id: "5", key: "5", name: "Beiersdorf" },
+                { id: "6", key: "6", name: "BMW" },
+                { id: "7", key: "7", name: "Continental" },
+                { id: "8", key: "8", name: "Covestro" },
+                { id: "9", key: "9", name: "Daimler" },
+                { id: "10", key: "10", name: "Deutsche Bank" },
+                { id: "11", key: "11", name: "Deutsche Börse" },
+                { id: "12", key: "12", name: "Deutsche Post" },
+                { id: "13", key: "13", name: "Deutsche Telekom" },
+                { id: "14", key: "14", name: "E.ON" },
+                { id: "15", key: "15", name: "Fresenius" },
+                { id: "16", key: "16", name: "Fresenius Medical Care" },
+                { id: "17", key: "17", name: "HeidelbergCement" },
+                { id: "18", key: "18", name: "Henkel" },
+                { id: "19", key: "19", name: "Infineon" },
+                { id: "20", key: "20", name: "Linde" },
+                { id: "21", key: "21", name: "Lufthansa" },
+                { id: "22", key: "22", name: "Merck" },
+                { id: "23", key: "23", name: "MTU Aero Engines" },
+                { id: "24", key: "24", name: "Münchener Rück" },
+                { id: "25", key: "25", name: "RWE" },
+                { id: "26", key: "26", name: "SAP" },
+                { id: "27", key: "27", name: "Siemens" },
+                { id: "28", key: "28", name: "Volkswagen" },
+                { id: "29", key: "29", name: "Vonovia" },
+                { id: "30", key: "30", name: "Wirecard" }],
             dropDownValue: 'Wähle ein Unternehmen ...',
             dropdownOpen: false
         };
@@ -83,6 +85,17 @@ export class Selection extends React.Component {
         var element = document.getElementById(id);
         element.classList.add("active");
         this.currentId = id;
+
+        if(this.currentId !== 0){
+            document.getElementById("ew").disabled = false;
+            document.getElementById("dcf").disabled = false;
+        }
+    }
+
+    handleClick = (e) => {
+        if (this.disableButton) {
+            e.preventDefault()
+        }
     }
 
     render() {
@@ -108,11 +121,11 @@ export class Selection extends React.Component {
                         <p>Methode der Berechnung</p>
 
                         <ButtonGroup vertical >
-                            <Button variant="light">
-                                <NavLink className="buttonlink" to="/result">Ertragswertverfahren</NavLink>
+                            <Button id="ew" disabled={this.disableButton} variant="light">
+                                <Link onClick={this.handleClick} className="buttonlink" to="/result">Ertragswertverfahren</Link>
                             </Button>
-                            <Button variant="light">
-                                <NavLink className="buttonlink" to="/result">Discounted Cashflow-Verfahren</NavLink>
+                            <Button id="dcf" disabled={this.disableButton} variant="light">
+                                <Link onClick={this.handleClick} className="buttonlink" to="/result">Discounted Cashflow-Verfahren</Link>
                             </Button>
                         </ButtonGroup>
                         {/*<br></br>
