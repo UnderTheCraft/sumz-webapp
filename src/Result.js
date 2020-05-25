f6d08b7c5161fe60e4de6330d49d03301f23f601
Domenic Nauert <dome777@me.com>
2020-05-25 16:59:40 +0200

    result init

---
+++ src/Result.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Layout } from './components/Layout';
+
+const Styles = styled.div`
+`;
+
+export const Result = () => (
+    <Styles>
+        <Layout>
+            <div className="App-body">
+                
+            </div>
+        </Layout>
+    </Styles>
+)
